079ea2d30121a673ff09a8909bec9096d2134d65
demobaba007 <ramodevsingh2222@gmail.com>
2025-02-20 23:51:43 +0530

    Modified src code with is-member and union function

--- first_prog.cpp
+++ first_prog.cpp
@@ -10,7 +10,7 @@ class SET {
 
     // Takes array and its lenght as parameter and returns normal set
     void Output(int *arrC, int len_C){
-        cout << "Displaying SET : \n" << "{ ";
+        cout << "Displaying SET : \n\n" << "{ ";
         for (i = 0; i < len_C; i++)
         cout << arrC[i] << " ";
         cout << "}\n";
@@ -35,13 +35,53 @@ class SET {
         cout << new_array[i] << " ";
     }
     cout << "}\n";
-
-        // Cardinality of set
-        cout << "Cardinality of Set : " << new_array.size() << endl;
         return new_array;
     }
 
     // Perform Is member function
+    bool Is_member(int Input_num, int *arrC, int len_C) {
+        bool flag;
+        for (int i = 0; i < len_C; i++){
+            if (arrC[i] == Input_num){
+                flag = 1;
+                break;
+            }else {
+                flag = 0;
+                continue;
+            }
+        }
+        return flag;
+    }
+
+    // Perform Union function
+    void Union(int *arrA, int len_A, int *arrB, int len_B){
+        vector<int> new_array3;
+        int i = 0;
+        new_array3.push_back(arrA[i]);
+        for (int i = 1; i < len_A; i++){
+            auto it = find(new_array3.begin(), new_array3.end(), arrA[i]);
+            if (it != new_array3.end()){
+                continue;
+            }else{
+                new_array3.push_back(arrA[i]);
+            }
+        }
+        for (int j = 0; j < len_B; j++){
+            auto it = find(new_array3.begin(), new_array3.end(), arrB[j]);
+            if (it != new_array3.end()){
+                continue;
+            }else{
+                new_array3.push_back(arrB[j]);
+            }
+        }
+        int len_C = new_array3.size();
+        cout << "Displaying Union of sets : \n\n" << "{ ";
+        for (i = 0; i < len_C; i++)
+        cout << new_array3[i] << " ";
+        cout << "}\n";
+        cout <<endl;
+        
+    }
 };
 
 int main() {
@@ -69,7 +109,7 @@ int main() {
     SET obj;
     for (int i = 0;i < 70; i++) cout << "_ ";
     cout << "\nWhat function do you want to perform in set : \n\n";
-    cout << "1. Output function \t\t\t 2. Unique function\n\n3. \n\n";
+    cout << "1. Output function \t\t\t 2. Unique function\n\n3. Is-member function \t\t\t 4. Union function\n\n5. Intersection function \t\t\t 6. \n\n";
     for (int i = 0; i < 70; i++) cout << "_ ";
 
     int user_input;
@@ -93,10 +133,38 @@ int main() {
         }else if (user_input == 2){
             for (int i = 0;i < 70; i++) cout << "_ "; cout << "\n\n";
             vector<int> new_array1 = obj.Unique(arrA, len_A);
+            // Cardinality of set
+            cout << "Cardinality of Set : " << new_array1.size() << endl;
             for (int i = 0;i < 70; i++) cout << "_ "; cout << "\n\n";
             vector<int> new_array2 = obj.Unique(arrB, len_B);
+            // Cardinality of set
+            cout << "Cardinality of Set : " << new_array2.size() << endl;
             for (int i = 0;i < 70; i++) cout << "_ "; cout << "\n\n";
+            break;
+
+            // Perform Is Member function
+        }else if (user_input == 3){
+            for (int i = 0;i < 70; i++) cout << "_ "; cout << "\n\n";
+            int Input_num;
+            cout << "Please enter the number you want to search : ";
+            cin >> Input_num;
+            int result = obj.Is_member(Input_num, arrA, len_A);
+            if (result == 1){
+                cout << Input_num << " is belongs to set\n";
+            }else{
+                cout << Input_num << " is not belongs to set\n";
+            }
+            for (int i = 0;i < 70; i++) cout << "_ "; cout << "\n\n";
+            break;
+
+            // Perform Union function
+        }else if (user_input == 4){
+            for (int i = 0;i < 70; i++) cout << "_ "; cout << "\n\n";
+            obj.Union(arrA, len_A, arrB, len_B);
+            for (int i = 0;i < 70; i++) cout << "_ "; cout << "\n\n";
+            break;
         }
+
     }
 
 
